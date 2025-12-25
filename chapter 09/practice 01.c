80b00fe6c4844ffd6d990af0a9b9f2b1e6ed8fd1
Amteshwar Singh <amteshwars8@gmail.com>
2025-12-25 18:31:21 +0530

    ch 9 and 10

---
+++ chapter 09/practice 01.c
@@ -0,0 +1,14 @@
+// create a 2D vector using structures in c
+
+#include <stdio.h>
+
+struct vector{
+    int i;
+    int j;
+};
+
+int main(){
+    struct vector v = {1, 2};
+    printf("The value of vector is %di + %d j", v.i, v.j);
+    return 0;
+}
